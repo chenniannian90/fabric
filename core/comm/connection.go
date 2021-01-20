bd1a15b2720fd5276849e8c0595a569bb3f64d5b
chenniannian <chenniannian@rockontrol.com>
2021-01-20 16:11:31 +0800

    aa

--- core/comm/connection.go
+++ core/comm/connection.go
@@ -21,7 +21,7 @@ import (
 	"github.com/hyperledger/fabric/common/flogging"
 	"github.com/hyperledger/fabric/core/config"
 	tls "github.com/tjfoc/gmtls"
-	"github.com/tjfoc/gmsm/gmtls/gmcredentials"
+	"github.com/tjfoc/gmtls/gmcredentials"
 	"github.com/tjfoc/gmsm/x509"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
